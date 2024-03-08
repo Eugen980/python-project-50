9d3d933cb4b858edc21d2922cc6a13240cb6db7d
Eugen980 <zaza980q@mail.ru>
2024-03-08 19:00:52 +0300

    08.03

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,14 +5,16 @@ from gendiff.generator import generate_diff
 @pytest.mark.parametrize(
     ['path_to_file1', 'path_to_file2', 'expected_file'],
     [
-        ('./tests/fixtures/file1.json',
-         './tests/fixtures/file2.json',
-         './tests/fixtures/expected.txt'
-         ),
-         ('./tests/fixtures/file1_yaml.yaml',
-         './tests/fixtures/file2_yaml.yml',
-         './tests/fixtures/expected_yaml.txt'
-         )
+        (
+            './tests/fixtures/file1.json',
+            './tests/fixtures/file2.json',
+            './tests/fixtures/expected.txt'
+        ),
+        (
+            './tests/fixtures/file1_yaml.yaml',
+            './tests/fixtures/file2_yaml.yml',
+            './tests/fixtures/expected_yaml.txt'
+        )
     ]
 )
 def test_generator_diff(path_to_file1, path_to_file2, expected_file):
